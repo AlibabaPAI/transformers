84a9579ef735df8dc3b8f5f45719e6e9df5cfe2f
shw <shw443569@alibaba-inc.com>
2024-07-29 17:33:24 +0800

    set fsdp stratety to autowrap

--- examples/pytorch/torchacc/llama3/run_acc.sh
+++ examples/pytorch/torchacc/llama3/run_acc.sh
@@ -47,6 +47,6 @@ torchrun --nproc_per_node $NPROC_PER_NODE \
     --gradient_checkpointing no \
     --logging_steps 100 \
     --$PRECISION \
-    --fsdp "full_shard" \
+    --fsdp "atuo_wrap" \
     --fsdp_config $FSDP_CONFIG 2>&1 | tee ./$JOB_NAME.log
 
